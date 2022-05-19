4caeb2dfa520324a04b913967742b030c48cc86f
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-05-19 20:54:30 +0200

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,30 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+    let repeatString = "";
+    
+    if (num > 0) {
+    while (num > 0) {
+        repeatString += string;
+        num--;
+        }
+    return repeatString;   
+    }
+    
+   else if (num == 0) {
+        return "";
+        }
 
-// Do not edit below this line
-module.exports = repeatString;
+    else if (num < 0){
+        return "ERROR";
+    }
+
+    }
+
+   
+    
+    repeatString('hey', 3); // returns 'heyheyhey'
+    
+    
+    
+    // Do not edit below this line
+    module.exports = repeatString;
