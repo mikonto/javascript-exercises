1898f86cb71cce0d6bc35bf6f932bd88667e7687
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-05-20 21:41:39 +0200

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fh) {
 
-};
-
-const ctof = function() {
-
-};
+  celcius = ((fh - 32) * (5/9)); // [°C] = ([°F] − 32) × 5⁄9
+  return Math.round(celcius * 10) / 10
+  
+  };
+  
+  const ctof = function(ce) {
+  fahrenheit = ((ce * (9/5)) + 32); // [°F] = [°C] × 9⁄5 + 32
+  return Math.round(fahrenheit * 10) / 10
+  
+  };
 
 // Do not edit below this line
 module.exports = {
