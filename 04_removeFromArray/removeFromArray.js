ef7e5685236b707ba885dde62345be11db742cac
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-05-20 17:27:06 +0200

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr1, ...arr2) {
+	return arr1.filter(test);
 
+    function test(item) {
+	return !arr2.includes(item)
+	}
 };
+
+removeFromArray([1, 2, 3, 4], 3, 4); // should remove 3 and return [1,2,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
