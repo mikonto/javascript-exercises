26ce29f2d130d9da3ed518d8d7bbba1ad63eb3c0
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-05-20 21:29:51 +0200

    Complete exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,27 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
-};
+    if (year % 4 === 0) { 
+        if (year % 400 === 0) { // If divisable by 400
+        return true;
+        }
+        
+        else if (year % 100 === 0) { // If divisable by 100
+        return false;
+        }
+        
+        else { // If divisable by 4
+        return true;
+        }
+    
+    }
+    
+    else {
+    return false;
+    }
+    
+    };
+    
+
 
 // Do not edit below this line
 module.exports = leapYears;
