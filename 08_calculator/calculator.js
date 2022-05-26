a37c6880eb8c2d22ae5d498fcb74078e81439b6c
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-05-26 21:06:30 +0200

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(ar) {
+  var total = 0;
+  for (var i = 0; i < ar.length; i++) {
+    total += ar[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(ar) {
+  var total = 1;
+  for (var i = 0; i < ar.length; i++) {
+    total *= ar[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+	if(n===0) {
+    return 1;
+  }
+  
+  else {}
+  return (n != 1) ? n * factorial(n - 1) : 1;
+}
+
 
 // Do not edit below this line
 module.exports = {
