b3232152e0a5f1d6af88c629cec6e74a65e106f4
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-05-27 12:23:50 +0200

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
-};
+const palindromes = function (text) {
+    let noCaps = text.toLowerCase(); 
+   
+    regex = /[a-z]/g;
+    let onlyLettersArray = noCaps.match(regex);
+    
+    
+    let onlyLetters = onlyLettersArray.join("");
+  
+    let reversed = onlyLetters.split("").reverse().join("");
+      console.log(reversed); // TEST
+      console.log(text); // TEST
+    
+    if (reversed === onlyLetters) {
+      return true
+      }
+    
+    else {
+      return false;
+      };
+          
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
